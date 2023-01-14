06b43d8868f9bf6285fdf8c351da2b4ffd6981ec
Léo Graciano <leonardoferreiragraciano@gmail.com>
2023-01-14 20:46:54 -0300

    test: ensure SignUpController return 400 if no email is provided

--- src/presentation/controlers/signup.spec.ts
+++ src/presentation/controlers/signup.spec.ts
@@ -15,4 +15,19 @@ describe('SingUp Controller', () => {
     expect(httpResponse.statusCode).toBe(400)
     expect(httpResponse.body).toEqual(new Error('Missing parameters: name'))
   })
+
+  test('should return 400 if no name is provided', () => {
+    const sut = new SingUpController()
+    const httpRequest = {
+      body: {
+        name: 'any_name',
+        // email: 'any_email@mail.com',
+        password: 'any_password',
+        passwordConfirmation: 'any_password'
+      }
+    }
+    const httpResponse = sut.handle(httpRequest)
+    expect(httpResponse.statusCode).toBe(400)
+    expect(httpResponse.body).toEqual(new Error('Missing parameters: email'))
+  })
 })
